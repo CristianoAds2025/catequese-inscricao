09002ee83113a047c290c1521cc4f36e068a5264
CristianoAds2025 <cristianodamiaodasilva@gmail.com>
2026-02-18 12:57:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
-from flask import Flask
-from flask import redirect
+from flask import Flask, redirect
 from config import Config
 from auth.routes import auth_bp
 from admin.routes import admin_bp
-
-@app.route('/')
-def home():
-    return redirect('/login')
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,8 +9,13 @@ app.config.from_object(Config)
 app.register_blueprint(auth_bp)
 app.register_blueprint(admin_bp)
 
+@app.route('/')
+def home():
+    return redirect('/login')
+
 if __name__ == "__main__":
     app.run()
+
 
 
 
