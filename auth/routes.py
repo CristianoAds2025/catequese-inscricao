1c7bbddead60186d77768328a924e1a07ac3c69e
CristianoAds2025 <cristianodamiaodasilva@gmail.com>
2026-02-18 14:30:23 -0400

    Update routes.py

--- auth/routes.py
+++ auth/routes.py
@@ -1,8 +1,29 @@
-from flask import Blueprint, render_template, request, redirect, session
+from flask import Blueprint, render_template, request, redirect, session, url_for
 from database.db import conectar_db
+from database import get_connection
 import bcrypt
 
 auth_bp = Blueprint('auth', __name__)
+
+from flask import render_template, session, redirect, url_for
+from database import get_connection
+
+@auth.route('/admin')
+def admin():
+    if 'usuario' not in session:
+        return redirect(url_for('auth.login'))
+
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("SELECT id, usuario FROM usuarios")
+    usuarios = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return render_template('admin.html', usuarios=usuarios)
+
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
