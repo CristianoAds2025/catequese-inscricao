d6d93e1e1b68daed715ee4c9d71c83e43dd7002f
CristianoAds2025 <cristianodamiaodasilva@gmail.com>
2026-02-18 12:31:18 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
+import os
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "supersecret")
+
+    DB_HOST = os.getenv("DB_HOST")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_NAME = os.getenv("DB_NAME")
+    DB_PORT = int(os.getenv("DB_PORT", 3306))
 
