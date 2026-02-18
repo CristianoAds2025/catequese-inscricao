601f15a63a1f1772953a95a8277690e415d1f580
CristianoAds2025 <cristianodamiaodasilva@gmail.com>
2026-02-18 12:33:08 -0400

    Update routes.py

--- admin/routes.py
+++ admin/routes.py
@@ -1,1 +1,17 @@
+from flask import Blueprint, render_template, session, redirect
+from database.db import conectar_db
+
+admin_bp = Blueprint('admin', __name__)
+
+@admin_bp.route('/admin')
+def painel():
+    if 'usuario' not in session:
+        return redirect('/login')
+
+    conexao = conectar_db()
+    cursor = conexao.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM inscricoes ORDER BY id DESC")
+    dados = cursor.fetchall()
+
+    return render_template('admin.html', dados=dados)
 
